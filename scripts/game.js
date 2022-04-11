4b8adb0185d17a43456d2e04f90d8dbfa14f94be
canozzcan <canozcanstu@gmail.com>
2022-04-11 20:21:39 +0300

    Managing Game Rounds and Selected Field

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,38 @@
 function startNewGame() {
-    if(players[0].name === '' || players[1].name === ''){
+    if (players[0].name === '' || players[1].name === '') {
         alert('Please set custom names fot both players!');
         return;
     }
 
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+    const selectedField = event.target;
+    const selectedColumn = +selectedField.dataset.col - 1;
+    const selectedRow = +selectedField.dataset.row - 1;
+
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        alert('Please select an empty field!');
+        return;
+    }
+
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add('disabled');
+
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+    console.log(gameData);
+
+    switchPlayer();
 }
